659ec749a660c1f338c841a8d5d244f0474f356c
Anil239009 <anilbemech2020@gmail.com>
2023-12-26 23:25:37 +0530

    Student Mangement using Spring data Crud Operations

--- Student_Course/src/main/java/com/app/dao/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Course;
-
-public interface CourseDao extends JpaRepository<Course, Long> {
-//find courses by category
-}
