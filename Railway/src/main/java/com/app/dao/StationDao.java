ab59c5bdd501ff590b89da79f0eef53977b23157
Anil239009 <anilbemech2020@gmail.com>
2023-12-27 22:05:29 +0530

    Railway Management using Spring data CRUD operations

--- Railway/src/main/java/com/app/dao/StationDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Station;
-
-public interface StationDao extends JpaRepository<Station, Long> {
-
-}
